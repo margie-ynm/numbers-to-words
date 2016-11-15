a8bc4fdd7e817902fe53991585f82ea44b089404
Joshua Rinard and Margie Nevarez <margie@>
2016-11-15 15:42:10 -0800

    adds numbers 1-19, and 20.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,8 @@
 class Fixnum
   define_method(:numbers_to_words) do
-  final_numbers = []
+
   ones = {
+    "0"=> "zero",
     "1"=> "one",
     "2"=> "two",
     "3"=> "three",
@@ -11,13 +12,62 @@ class Fixnum
     "7"=> "seven",
     "8"=> "eight",
     "9"=> "nine",
-
+    "11"=> "eleven",
+    "12"=> "twelve",
+    "13"=> "thirteen",
+    "14"=> "fourteen",
+    "15"=> "fifteen",
+    "16"=> "sixteen",
+    "17"=> "seventeen",
+    "18"=> "eighteen",
+    "19"=> "nineteen",
   }
 
-  split_number = self.to_s.reverse().split("")
-  split_number.each() do |n|
-  final_numbers.push(ones.fetch(n))
-  end
-  final_numbers.join("")
-end
-end
+  doubles = {
+    "20"=> "twenty",
+    "30"=> "thirty",
+    "40"=> "forty",
+    "50"=> "fifty",
+    "60"=> "sixty",
+    "70"=> "seventy",
+    "80"=> "eighty",
+    "90"=> "ninety",
+  }
+
+  final_numbers = []
+
+  i = 0
+
+  if self < 20
+    word = ones.fetch(self.to_s())
+    final_numbers.push(word)
+    puts word
+  elsif self == 20
+    split_number = self.to_s.split("")
+    # split_number.each() do |n|
+    number = split_number[0].to_i.*(10)
+    puts number
+    word = doubles.fetch(number.to_s())
+    final_numbers.push(word)
+
+    # i+=1
+
+
+      # elsif self < 20 and self > 9 and i < 1
+      #   word = tens.fetch(n)
+      #   final_numbers.push(word)
+    # elsif self >= 20 and self < 100 and i < 1
+    #
+    #     word = doubles.fetch(n)
+    #     puts word
+    #     final_numbers.push(word)
+    #
+    #     word2 = ones.fetch(split_number[i + 1])
+    #     final_numbers.push(word2)
+    #     i +=1
+    #
+      # end # if end
+    end # each end
+    final_numbers.join(" ")
+  end # method end
+end #class end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,8 +3,11 @@ require('pry')
 require('numbers_to_words.rb')
 
 describe('Fixnum#numbers_to_words') do
-  it('returns word for a number') do
+  it('returns word for a single digit number 1 19') do
     expect(4.numbers_to_words()).to(eq("four"))
+  end
+  it('returns word for a number 20 99') do
+    expect(20.numbers_to_words()).to(eq("twenty"))
   end
 
 end
